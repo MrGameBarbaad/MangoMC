e858538525a1366ee6e5f046becb94a8ca6869cf
MrGameBarbaad <autocreatedparamjitsingh@gmail.com>
2026-01-19 21:33:36 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 console.log("Homepage loaded");
+
+fetch("https://api.mcsrvstat.us/2/mangomc.info.gf")
+  .then(res => res.json())
+  .then(data => {
+    const el = document.getElementById("mc-count");
+    if (!el) return;
+
+    if (data.online) {
+      el.textContent = data.players.online;
+    } else {
+      el.textContent = "0";
+    }
+  })
+  .catch(() => {
+    document.getElementById("mc-count").textContent = "0";
+  });
